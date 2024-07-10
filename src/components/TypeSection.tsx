fbd75b1f9f76f299427d992a7bb9c4f3e33ae1be
Sahildhillon0 <sahildhillon609@gmail.com>
2024-07-11 00:01:52 +0530

    solving white issue

--- src/components/TypeSection.tsx
+++ src/components/TypeSection.tsx
@@ -21,10 +21,10 @@ function TypeSection() {
         },
     ];
     return (
-        <div className="flex flex-col items-center justify-center h-[10rem] mb-12 sm:-mt-20 -mt-36 w-full">
+        <div className="bg-black flex flex-col items-center justify-center h-[10rem] mb-12 sm:-mt-20 -mt-36 w-full">
             <TypewriterEffectSmooth words={words} />
-            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-                <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 bg-black">
+                <button className="w-40 h-10 rounded-xl bg-black border border-white border-transparent text-white text-sm">
                     Join now
                 </button>
             </div>
